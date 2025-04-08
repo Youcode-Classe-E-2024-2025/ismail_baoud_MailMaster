6bf4e99ec6646733cbfdd3b89f16940037dc137c
ismailbaoud <ibaoud46@gmail.com>
2025-04-08 09:36:08 +0100

    ajouter la documentation de base dans le controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,25 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+/**
+ * @OA\Info(
+ *     title="MailMaster API",
+ *     version="1.0.0",
+ *     description="API documentation for the MailMaster system",
+ *     @OA\Contact(
+ *         email="support@mailmaster.com"
+ *     ),
+ *     @OA\License(
+ *         name="MIT",
+ *         url="https://opensource.org/licenses/MIT"
+ *     )
+ * )
+ */
+
+class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, ValidatesRequests;
 }
