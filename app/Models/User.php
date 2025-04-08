abc96c4c16a03c78ead9e4a42d2f01fc778f4ea6
ismailbaoud <ibaoud46@gmail.com>
2025-04-08 09:07:10 +0100

    modifier le modile user et ajouter les relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+
+    
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable,HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -45,4 +48,17 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function newsletters() {
+        return $this->hasMany(Newsletter::class);
+    }
+    
+    public function subscribers() {
+        return $this->hasMany(Subscriber::class);
+    }
+    
+    public function campaigns() {
+        return $this->hasMany(Campaign::class);
+    }
+    
 }
