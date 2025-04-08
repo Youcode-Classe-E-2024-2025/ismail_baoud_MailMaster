79fec1678ad22daeee14d58f424f9bf1dd33b49f
ismailbaoud <ibaoud46@gmail.com>
2025-04-08 09:32:30 +0100

    ajouter la method logout avec la logique de logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,36 +7,9 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
 
+
 class AuthController extends Controller
 {
-
-    public function register(Request $request)
-    {
-        // Validate the incoming data
-        $fields = $request->validate([
-            'name' => 'required|string',
-            'email' => 'required|string|email|unique:users,email',
-            'password' => 'required|string|confirmed',
-        ]);
-
-        // Create the user and hash the password
-        $user = User::create([
-            'name' => $fields['name'],
-            'email' => $fields['email'],
-            'password' => bcrypt($fields['password']),
-        ]);
-
-        // Generate token for the user
-        $token = $user->createToken('mailmaster-token')->plainTextToken;
-
-        // Respond with the user data and token
-        return response()->json([
-            'message' => 'User created successfully',
-            'user' => $user,
-            'token' => $token,
-        ], 201);
-    }
-
 
     public function login(Request $request)
     {
@@ -61,5 +34,14 @@ class AuthController extends Controller
         ], 200);
     }
 
+
+public function logout(Request $request)
+{
+    $request->user()->tokens()->delete();
+
+    return response()->json([
+        'message' => 'Logged out successfully'
+    ]);
+}
 
 }
