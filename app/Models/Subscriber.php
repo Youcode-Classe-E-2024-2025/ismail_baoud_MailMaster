88f16b4ed65e4e4267bedb19ffa8d332249747fe
ismailbaoud <ibaoud46@gmail.com>
2025-04-16 11:06:19 +0100

    ajouter les field fillable de model subscriber

--- app/Models/Subscriber.php
+++ app/Models/Subscriber.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subscriber extends Model
 {
+
+    protected $fillable = [
+        "email",
+        "user_id",
+        "name"
+    ];
     public function user() {
         return $this->belongsTo(User::class);
     }
